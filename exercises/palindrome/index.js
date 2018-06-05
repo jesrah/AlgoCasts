7f01ab177d3c5935ae5e640a3eac822cb488339d
jesrah <jesrah@gmail.com>
2018-06-05 11:01:19 -0700

    Added node v 10 and solved palindrome and reversestring

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,22 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+	return str.split('').every((char, i) => {
+		return char === str[str.length - i - 1];
+	})
+}
 
 module.exports = palindrome;
+
+// //Original Solution
+// function palindrome(str) {
+// 	return str === str.split('').reduce((reverse, char) => char + reverse, '') ? true : false
+
+// 	//the true: false after ? is actually unnecessary bc returning the equality statement already returns true or false
+// }
+
+// //Solution using reverse()
+// function palindrome(str) {
+// 	return str === str.split('').reverse().join('');
+// }
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	// debugger;
+	return str.split('').reduce((reversed, char) => char + reversed, '')
+}
+
+// reverse('kishmish');
 
 module.exports = reverse;
+
+// function reverse(str) {
+// 	return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+// 	let result = '';
+// 	for (let char of str) {
+// 		result = char + result;
+// 	}
+// 	return result;
+// }
+
+// function reverse(str) {
+// 	let result = '';
+// 	//this kind of for loop can introduce a lot of room for error
+// 	//better to use the for...of syntax where possible
+// 	for (let i = str.length - 1; i >= 0; i--) {
+// 		result = result.concat(str[i])
+// 	}
+// 	return result;
+// }
